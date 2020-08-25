243a8bc0969b160f5693e89bd4d53d7de860a507
HarshKhandeparkar <mjkgoa@gmail.com>
2020-08-25 16:44:30 +0530

    fix: remove offset

--- shadowllax.js
+++ shadowllax.js
@@ -1,6 +1,5 @@
 // Parallax
 const depthFactor = 5; // Depth of the image wrt to the 0 z-index. Infinity being at the same distance as the rest of the content and 1 being as far away as possible.
-const parallaxOffset = 20;
 
 const resizeParallax = () => {
   document.querySelectorAll('.parallax').forEach(parallax => {
@@ -24,13 +23,11 @@ window.addEventListener('resize', resizeParallax)
 const reposition = () => {
   const movedBy = window.scrollY;
   document.querySelectorAll('.parallax').forEach(parallax => {
-    if (document.documentElement.clientHeight - parallax.getBoundingClientRect().top >= parallaxOffset) {
-      parallax.querySelectorAll('.parallax-layer').forEach(layer => {
-        const depth = Number(layer.getAttribute('data-depth-factor')) || depthFactor; // dynamically get depthFactor
+    parallax.querySelectorAll('.parallax-layer').forEach(layer => {
+      const depth = Number(layer.getAttribute('data-depth-factor')) || depthFactor; // dynamically get depthFactor
 
-        if (document.documentElement.clientHeight >= layer.getBoundingClientRect().top)layer.style.transform = `translateY(+${movedBy / depth}px)`; // Move differently than the rest of the window
-      })
-    }
+      if (document.documentElement.clientHeight >= layer.getBoundingClientRect().top)layer.style.transform = `translateY(+${movedBy / depth}px)`; // Move differently than the rest of the window
+    })
   })
 
   window.requestAnimationFrame(reposition);
