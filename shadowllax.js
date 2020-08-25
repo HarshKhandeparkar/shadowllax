d9d2c8438699fed336d08161a1db631de0530d3a
HarshKhandeparkar <mjkgoa@gmail.com>
2020-08-25 13:10:29 +0530

    feat: finalize library

---
+++ shadowllax.js
@@ -0,0 +1,39 @@
+// Parallax
+const depthFactor = 5; // Depth of the image wrt to the 0 z-index. Infinity being at the same distance as the rest of the content and 1 being as far away as possible.
+const parallaxOffset = 20;
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.parallax').forEach(parallax => {
+    let height = 0; // To take into account absolute elements;
+
+    parallax.querySelectorAll('.parallax-layer').forEach(layer => {
+      const changeHeight = () => {
+        height = Math.max(height, layer.clientHeight);
+
+        if(height > parallax.clientHeight) parallax.style.height = `${height}px`;
+        console.log(height)
+      }
+
+      if (layer.tagName == 'IMG') layer.addEventListener('load', changeHeight);
+      else changeHeight();
+    })
+  })
+})
+
+const reposition = () => {
+  const movedBy = window.scrollY;
+  document.querySelectorAll('.parallax').forEach(parallax => {
+    if (document.documentElement.clientHeight - parallax.getBoundingClientRect().top >= parallaxOffset) {
+      parallax.querySelectorAll('.parallax-layer').forEach(layer => {
+        const depth = Number(layer.getAttribute('data-depth-factor')) || depthFactor; // dynamically get depthFactor
+
+        if (document.documentElement.clientHeight >= layer.getBoundingClientRect().top)layer.style.transform = `translateY(+${movedBy / depth}px)`; // Move differently than the rest of the window
+      })
+    }
+  })
+
+  window.requestAnimationFrame(reposition);
+}
+
+window.requestAnimationFrame(reposition);
+// /Parallax
