4b4d73484c7df16564e1904df013612b400f26cd
HarshKhandeparkar <mjkgoa@gmail.com>
2020-09-30 01:51:08 +0530

    fix: js code optimization and fixes

--- shadowllax.js
+++ shadowllax.js
@@ -17,17 +17,28 @@ const resizeParallax = () => {
     })
   })
 }
-window.addEventListener('DOMContentLoaded', resizeParallax)
-window.addEventListener('resize', resizeParallax)
+
+const getInitialHeights = () => {
+  document.querySelectorAll('.parallax-layer').forEach(layer => {
+    layer.setAttribute('data-initial-height', layer.getBoundingClientRect().top + window.pageYOffset)
+  })
+}
 
 const reposition = () => {
-  const movedBy = window.scrollY;
   document.querySelectorAll('.parallax').forEach(parallax => {
     parallax.querySelectorAll('.parallax-layer').forEach(layer => {
-      const depth = Number(layer.getAttribute('data-depth-factor')) || depthFactor; // Dynamically get depthFactor
+      const depth = Number(layer.getAttribute('data-depth-factor') || depthFactor); // Dynamically get depthFactor
+      const initialLayerHeight = Number(layer.getAttribute('data-initial-height'));
+      let moveLayer = window.scrollY / depth; // The amount the layer should be moved by
 
-      if (document.documentElement.clientHeight >= layer.getBoundingClientRect().top) { // Don't move offscreen elements.
-        layer.style.transform = `translateY(+${movedBy / depth}px)`; // Move differently than the rest of the window
+      if (initialLayerHeight > document.documentElement.clientHeight) moveLayer -= (initialLayerHeight - document.documentElement.clientHeight) / depth;
+
+      const renderedLayerTop = layer.getBoundingClientRect().top + moveLayer;
+      const renderedLayerBottom = layer.getBoundingClientRect().bottom + moveLayer;
+
+      if (renderedLayerTop <= document.documentElement.clientHeight && renderedLayerBottom >= 0) { // Only translate if the final element will be on screen
+        console.log('moved' + layer.className)
+        layer.style.transform = `translateY(${moveLayer}px)`; // Move differently than the rest of the window
       }
     })
   })
@@ -35,5 +46,9 @@ const reposition = () => {
   window.requestAnimationFrame(reposition);
 }
 
-window.requestAnimationFrame(reposition);
-// /Parallax
+window.addEventListener('DOMContentLoaded', () => {
+  resizeParallax();
+  getInitialHeights();
+  window.requestAnimationFrame(reposition);
+})
+window.addEventListener('resize', resizeParallax);
